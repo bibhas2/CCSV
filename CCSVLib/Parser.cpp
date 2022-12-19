2492d4fbacc70a4293a47a0c20f340aaee849f3c
Bibhas Bhattacharya <bibhas2@gmail.com>
2022-12-19 17:53:10 -0500

    Improve parsing

--- CCSVLib/Parser.cpp
+++ CCSVLib/Parser.cpp
@@ -1,6 +1,12 @@
 #include "Parser.h"
 
 namespace ccsv {
+enum ParseStatus {
+    HAS_MORE_FIELDS,
+    END_RECORD,
+    END_DOCUMENT
+};
+
 void eat_space(Reader& reader) {
     char ch = reader.pop();
 
@@ -15,59 +21,84 @@ void eat_space(Reader& reader) {
     reader.putback();
 }
 
-bool parse_record(Reader& reader, std::span<std::string_view> storage, std::span<std::string_view>& record) {
-    size_t field_index = 0;
+ParseStatus next_field(Reader& reader, std::string_view& field) {
+    reader.mark_start();
     
-    while (field_index < storage.size()) {
-        reader.mark_start();
+    while (true) {
+        char ch = reader.pop();
         
-        while (true) {
-            char ch = reader.peek();
+        if (ch == '\0') {
+            reader.mark_end();
+            field = reader.segment();
             
-            if (ch == '\0') {
-                //End of data
-                return false;
-            }
+            return END_DOCUMENT;
+        }
+        
+        if (ch == ',') {
+            reader.putback();
+            reader.mark_end();
+            field = reader.segment();
+            reader.pop();
             
-            if (ch == ',' || ch == '\r') {
-                //End of field.
-                reader.mark_end();
-                
-                storage[field_index] = reader.segment();
-                
-                //Eat the last char
-                reader.pop();
-                
-                if (ch == ',') {
-                    //We have more fields
-                    ++field_index;
-                    break;
-                } else {
-                    //Read \n
-                    reader.pop();
-
-                    //End of record
-                    record = storage.subspan(0, field_index + 1);
-
-                    return true;
-                }
-            } else {
-                reader.pop();
-            }
+            return HAS_MORE_FIELDS;
+        }
+        
+        if (ch == '\r') {
+            reader.putback();
+            reader.mark_end();
+            field = reader.segment();
+            reader.pop();
+            reader.pop(); //Read \n
+            
+            return END_RECORD;
+        }
+        
+        /*
+         * Non-standard end of line with just a \n
+         */
+        if (ch == '\n') {
+            reader.putback();
+            reader.mark_end();
+            field = reader.segment();
+            reader.pop();
+            
+            return END_RECORD;
         }
     }
+}
+
+ParseStatus parse_record(Reader& reader, std::span<std::string_view> storage, std::span<std::string_view>& record) {
+    size_t field_index = 0;
+    std::string_view field;
     
-    //We should not come here
+    while (true) {
+        auto status = next_field(reader, field);
+        
+        if (status == END_DOCUMENT) {
+            return status;
+        }
+        
+        if (field_index < storage.size()) {
+            storage[field_index] = field;
+
+            ++field_index;
+        }
+        
+        if (status == END_RECORD) {
+            record = storage.subspan(0, field_index);
+            
+            return status;
+        }        
+    }
+    
     assert(false);
-    
-    return false;
 }
 
 void _parse(Reader& reader, std::span<std::string_view> storage, std::function<void(size_t, std::span<std::string_view>)> consumer) {
     std::span<std::string_view> record;
     size_t index = 0;
     
-    while (parse_record(reader, storage, record)) {
+    while (parse_record(reader, storage, record) != END_DOCUMENT) {
         consumer(index, record);
         
         ++index;
--- Test/main.cpp
+++ Test/main.cpp
@@ -20,6 +20,50 @@ void test_record() {
     });
 }
 
+void test_newline() {
+    //Non-standard CSV but common in Linux.macOS
+    auto str = "aa,bb,cc,dd\nee,ff,gg,hh\n";
+    ccsv::StringReader reader(str);
+
+    ccsv::parse<10>(reader, [](size_t index, std::span<std::string_view> fields) {
+        assert(index < 2);
+        
+        if (index == 0) {
+            assert(fields[0] == "aa");
+            assert(fields[3] == "dd");
+        } else {
+            assert(fields[0] == "ee");
+            assert(fields[3] == "hh");
+        }
+    });
+}
+
+void test_uneven() {
+    auto str = "aa,bb,cc,dd\r\nee,ff,gg\r\nhh,ii\r\n";
+    ccsv::StringReader reader(str);
+
+    ccsv::parse<3>(reader, [](size_t index, std::span<std::string_view> fields) {
+        assert(index < 3);
+        
+        if (index == 0) {
+            assert(fields.size() == 3);
+            
+            assert(fields[0] == "aa");
+            assert(fields[2] == "cc");
+        } else if (index == 1) {
+            assert(fields.size() == 3);
+            
+            assert(fields[0] == "ee");
+            assert(fields[1] == "ff");
+        } else {
+            assert(fields.size() == 2);
+            
+            assert(fields[0] == "hh");
+            assert(fields[1] == "ii");
+        }
+    });
+}
+
 void test_string_reader() {
     auto str = "aa,bb,cc,dd";
     ccsv::StringReader reader(str);
@@ -44,6 +88,8 @@ void test_string_reader() {
 int main() {
     test_string_reader();
     test_record();
+    test_uneven();
+    test_newline();
     
     return 0;
 }
